c332d12195bd8212ef09b3c383bda1f4fd6e3f07
kasi <danielkaser@mailbox.org>
2023-04-27 09:12:39 +0200

    add button animation

---
+++ index.js
@@ -0,0 +1,9 @@
+const showAnswerButtons = document.querySelectorAll(
+  '[data-js="show-answer-button"'
+);
+
+for (button of showAnswerButtons) {
+  button.addEventListener("click", () => {
+    answer.classList.toggle("hidden");
+  });
+}
