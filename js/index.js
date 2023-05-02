cba7b3e73be4a65a0d38206306c64f41bcf41d4c
kasi <danielkaser@mailbox.org>
2023-05-02 10:52:49 +0200

    finish show and hide answer functionality

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 const bookmark = document.querySelector('[data-js="bookmark"]');
+const showAnswerButton = document.querySelector(
+  '[data-js="show-answer-button"]'
+);
+const answer = document.querySelector('[data-js="answer"]');
 
-// console.log("test:", bookmark);
+// console.log("test:", answer);
 
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("fa-solid");
   bookmark.classList.toggle("fa-regular");
   //   console.log("bookmark:", bookmark);
+});
+
+showAnswerButton.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
 });
